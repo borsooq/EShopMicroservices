c0b207e4f5626076c6fd4340b2b9772e35bc8b3d
Mirko Boruc <borsooq@gmail.com>
2024-09-15 17:56:53 +0200

    Added PostgreSQL in docker

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,10 +6,14 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
-app.MapCarter();
 
 //Add HTTP request pipelines
+app.MapCarter();
 
 app.Run();
