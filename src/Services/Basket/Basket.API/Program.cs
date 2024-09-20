bab2a293178bcf157108d69578dcd5c14c4dffd6
Mirko Boruc <borsooq@gmail.com>
2024-09-20 20:12:15 +0200

    Added Delete action and registrations

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the conatiner
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
 //configure http request pipeline
+app.MapCarter();
 
 app.Run();
