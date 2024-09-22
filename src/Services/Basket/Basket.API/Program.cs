583a6934cc1e028e49dc1a4ca6fdd5daa7067705
Mirko Boruc <borsooq@gmail.com>
2024-09-22 20:36:38 +0200

    Added health status functionality to Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,7 @@
 using Basket.API.Data;
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,11 +32,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 //configure http request pipeline
 app.MapCarter();
 app.UseExceptionHandler(option => { });
+app.UseHealthChecks("/health", new HealthCheckOptions { 
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 
 app.Run();
