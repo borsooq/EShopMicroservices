dcb75ac48c491045217297e191cf58fe7e267c31
Mirko Boruc <borsooq@gmail.com>
2024-10-05 21:15:15 +0200

    Added EF Core and definitions for some models

---
+++ src/Services/Ordering/Ordering.Infrastructure/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<OrderAggregate> Oders => Set<OrderAggregate>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Scans the assemmbly and applies according to the models
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
